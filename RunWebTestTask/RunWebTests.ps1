c2369b25565906fa84d062aa224991e8c2cabf50
Ricardo Serradas <riserrad@microsoft.com>
2017-09-29 10:38:30 -0300

    Adding some validation to the powershell script

--- RunWebTestTask/RunWebTests.ps1
+++ RunWebTestTask/RunWebTests.ps1
@@ -1,25 +1,53 @@
 param(
-    [string]$webtestlocation,
-    [string]$resultslocation,
-    [string]$testsettingslocation,
-    [bool]$publistestresults
+    [string]$WebTestLocation,
+    [string]$ResultsLocation,
+    [string]$TestSettingsLocation,
+    [switch]$PublisTestResults,
+    [switch]$OverWriteTestResults
 )
 
-$files = Get-ChildItem $webtestlocation -Filter "*.webtest"
-$paramstring = ""
+# Checks if the folder of the path for the Test Results exists. If not, creates it.
+$testresultsfolder = Split-Path $ResultsLocation
 
-foreach($webtest in $files){
-    $paramstring = $paramstring + "/testcontainer:""$webtestlocation\" + $webtest + """ "
+if ($PublisTestResults) {
+    Write-Warning "Publish Test Results set to true but feature still not implemented."
 }
 
-if (-not ([string]::IsNullOrEmpty($testsettingslocation))) {
-    if (Test-Path $testsettingslocation) {
-        $paramstring = $paramstring + "/testsettings:""" + $testsettingslocation + """ "
+# Checking wether Test Results file exists or not. If Overwrite set to true, removes it to be recreated after.
+if (-Not (Test-Path $testresultsfolder -PathType Container)) {
+    Write-Warning "Folder $testresultsfolder does not exist. Creating..."
+    New-Item $testresultsfolder -Type Directory
+}
+elseif (Test-Path $ResultsLocation) {
+    if ($OverWriteTestResults) {
+        Write-Warning "The Test Results file $ResultsLocation already exists."
+        Write-Warning "OverwriteTestResults set to true. Replacing it..."
+        Remove-Item "$testresultsfolder\\*" -Recurse
+    }
+    else {
+        Write-Error "The Test Results file $ResultsLocation already exists."
+    }
+}
+
+# Retrieves all the web test files under the specified folder
+$files = Get-ChildItem $WebTestLocation -Filter "*.webtest"
+$paramstring = ""
+
+# Adds all web test files to an array
+foreach($webtest in $files){
+    $paramstring = $paramstring + "/testcontainer:""$WebTestLocation\" + $webtest + """ "
+}
+
+# If specified, checks if the Test Settings file path is valid. If not, switches not to use Test Settings file.
+if (-not ([string]::IsNullOrEmpty($TestSettingsLocation))) {
+    if (Test-Path $TestSettingsLocation) {
+        $paramstring = $paramstring + "/testsettings:""" + $TestSettingsLocation + """ "
     }
     else {
         Write-Warning "You specified an invalid Test Settings location. Switching to run without a Test Settings file."
     }
 }
 
-$cmd = '& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" ' + $paramstring + '/usestderr /resultsfile:"' + $resultslocation + '"'
-Write-Host $cmd
+$cmd = '& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" ' + $paramstring + '/usestderr /resultsfile:"' + $ResultsLocation + '"'
+#Write-Host $cmd
+Invoke-Expression $cmd
