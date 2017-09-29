e2ba0b2e09c2d1f91195a98f69985e10a9e0cbaf
Ricardo Serradas <riserrad@microsoft.com>
2017-09-28 21:15:29 -0300

    Changing the powershell to build the command

--- RunWebTestTask/RunWebTests.ps1
+++ RunWebTestTask/RunWebTests.ps1
@@ -4,3 +4,22 @@ param(
     [string]$testsettingslocation,
     [bool]$publistestresults
 )
+
+$files = Get-ChildItem $webtestlocation -Filter "*.webtest"
+$paramstring = ""
+
+foreach($webtest in $files){
+    $paramstring = $paramstring + "/testcontainer:""$webtestlocation\" + $webtest + """ "
+}
+
+if (-not ([string]::IsNullOrEmpty($testsettingslocation))) {
+    if (Test-Path $testsettingslocation) {
+        $paramstring = $paramstring + "/testsettings:""" + $testsettingslocation + """ "
+    }
+    else {
+        Write-Warning "You specified an invalid Test Settings location. Switching to run without a Test Settings file."
+    }
+}
+
+$cmd = '& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" ' + $paramstring + '/usestderr /resultsfile:"' + $resultslocation + '"'
+Write-Host $cmd
